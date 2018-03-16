1dc9ee0bd6a8299d3d97257c8fb38d6f932e1ca6
Bernat <bernat.orellana@gmail.com>
2018-03-16 16:52:50 +0100

    bernat

--- ChartAdvancedStockLine/src/chartadvancedstockline/Nova.java
+++ ChartAdvancedStockLine/src/chartadvancedstockline/Nova.java
@@ -1,1 +1,27 @@
-//Acabo de crear nova.java
+//Més cosetes fetes pel profe diabòlic !!!!!
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chartadvancedstockline;
+
+import com.dayofthebeast.Beast;
+
+/**
+ *	Més cosetes fetes pel profe diabòlic !!!!!
+ * @author Usuari
+ */
+public class Nova {
+
+	//Més cosetes fetes pel profe diabòlic !!!!!
+
+	//Acabo de crear nova.java
+	
+	//Més cosetes fetes pel profe diabòlic !!!!!
+
+	//Més cosetes fetes pel profe diabòlic !!!!!
+
+}
+
