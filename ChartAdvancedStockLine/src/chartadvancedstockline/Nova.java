aa3d86e8b0b546b8adb34843793ad32f48c19b25
Bernat <bernat.orellana@gmail.com>
2018-03-16 16:30:39 +0100

    bernat

--- ChartAdvancedStockLine/src/chartadvancedstockline/Nova.java
+++ ChartAdvancedStockLine/src/chartadvancedstockline/Nova.java
@@ -5,12 +5,19 @@
  */
 package chartadvancedstockline;
 
+import com.dayofthebeast.Beast;
+
+
 /**
  *
  * @author Usuari
  */
 public class Nova {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+		//Més cosetes fetes pel profe diabòlic !!!!!
+
+        System.out.println("Hello World 666");
+		//Més cosetes fetes pel profe diabòlic !!!!!
+
     }
 }
