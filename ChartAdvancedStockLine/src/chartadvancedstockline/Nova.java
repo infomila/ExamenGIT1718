1126b53cd7c31143f592dd84570c9d0e447b828d
CirnoTheNinth <ultimatelink9@gmail.com>
2018-03-16 15:56:42 +0100

    clase Nova creada

---
+++ ChartAdvancedStockLine/src/chartadvancedstockline/Nova.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chartadvancedstockline;
+
+/**
+ *
+ * @author Erik
+ */
+public class Nova {
+    //son una clase inutil
+}
